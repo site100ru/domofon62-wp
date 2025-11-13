503c295e4fd0144ba268349e0729c65dc335a54b
Slava Sidorov <sidorov-vv3@mail.ru>
2025-11-13 22:03:14 +0500

    проверяю видео

--- page-about.php
+++ page-about.php
@@ -111,12 +111,13 @@
 				<div class="row">
 					<div class="col">
 						<div style="position: relative">
-							<video id="video-player" poster="<?php echo get_template_directory_uri(); ?>/img/video-poster-img.jpg" playsinline="playsinline" loop="loop">
+							<video id="video-player" poster="<?php echo get_template_directory_uri(); ?>/img/video-poster-img.jpg"  playsinline="playsinline" loop="loop" muted="" autoplay="">
 								<!-- muted="muted" autoplay="autoplay" -->
 								<!-- <source src="header-bg.ogv" type='video/ogg; codecs="theora, vorbis"'> -->
 								<source  src="<?php echo get_template_directory_uri(); ?>/video/video.mp4"  type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"'/>
 								<!-- <source src="header-bg.webm" type='video/webm; codecs="vp8, vorbis"'> -->
 							</video>
+
 							<div id="play-circle">
 								<div id="play" onclick="play();"></div>
 							</div>
